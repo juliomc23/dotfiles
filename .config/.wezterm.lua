1942ba94305071f0f4d242946dc1951b196889e3
Julio Macias Caldera <95148729+juliomc23@users.noreply.github.com>
2025-02-11 13:02:05 +0100

    add wezterm file

---
+++ .config/.wezterm.lua
@@ -0,0 +1,19 @@
+local wezterm = require("wezterm")
+local config = wezterm.config_builder()
+
+config.font = wezterm.font("JetBrains Mono")
+config.font_size = 16
+
+config.enable_tab_bar = false
+config.window_decorations = "RESIZE"
+
+config.color_scheme = "Catppuccin Mocha"
+config.window_background_opacity = 0.95
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
+config.default_domain = "WSL:Ubuntu"
+return config
