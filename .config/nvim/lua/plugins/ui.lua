a94df71e4f8dd4e9e3f1d6b787985a4f544063ab
juliomc23 <julito2302mc@gmail.com>
2025-02-12 20:03:43 +0100

    feat: add new changes

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -65,19 +65,39 @@ return {
         lualine_a = {
           "mode", -- Display the current mode
         },
-        lualine_b = {
-          "branch",
-        },
-        lualine_c = {
-          "filename",
-        },
+        lualine_b = { "branch" },
+        lualine_c = {},
         lualine_x = {},
         lualine_y = {},
         lualine_z = {},
       },
     },
   },
+  -- Plugin: incline.nvim
+  -- URL: https://github.com/b0o/incline.nvim
+  -- Description: A Neovim plugin for showing the current filename in a floating window.
+  {
+    "b0o/incline.nvim",
+    event = "BufReadPre", -- Load this plugin before reading a buffer
+    priority = 1200, -- Set the priority for loading this plugin
+    config = function()
+      require("incline").setup({
+        window = { margin = { vertical = 0, horizontal = 1 } }, -- Set the window margin
+        hide = {
+          cursorline = true, -- Hide the incline window when the cursorline is active
+        },
+        render = function(props)
+          local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t") -- Get the filename
+          if vim.bo[props.buf].modified then
+            filename = "[+] " .. filename -- Indicate if the file is modified
+          end
 
+          local icon, color = require("nvim-web-devicons").get_icon_color(filename) -- Get the icon and color for the file
+          return { { icon, guifg = color }, { " " }, { filename } } -- Return the rendered content
+        end,
+      })
+    end,
+  },
   -- Plugin: mini.nvim
   -- URL: https://github.com/echasnovski/mini.nvim
   -- Description: A collection of minimal, fast, and modular Lua plugins for Neovim.
