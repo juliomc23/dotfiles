216c9d492b7600592e1207a1d82fa0588d7cd53e
juliomc23 <julito2302mc@gmail.com>
2025-03-07 12:36:49 +0100

    feat: modify snacks

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -99,13 +99,33 @@ return {
   {
     "folke/snacks.nvim",
     opts = {
+      image = {},
+      picker = {
+        matcher = {
+          fuzzy = true,
+          smartcase = true,
+          ignorecase = true,
+          filename_bonus = true,
+        },
+        sources = {
+          explorer = {
+            matcher = {
+              fuzzy = true, -- Enables fuzzy matching, so you can be a bit imprecise with your search terms
+              smartcase = true, -- If your search term has uppercase letters, the search becomes case-sensitive
+              ignorecase = true, -- Ignores case when searching, unless smartcase is triggered
+              filename_bonus = true, -- Gives a higher priority to matches in filenames
+              sort_empty = false, -- If no matches are found, it won't sort the results
+            },
+          },
+        },
+      },
       dashboard = {
         sections = {
           { section = "header", pane = 2 },
           { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
           { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
           { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
-          -- { section = "startup" },
+          { section = "startup" },
         },
         preset = {
           header = [[
