de45f266e0a4325d8709e56296db47398eea4dd1
juliomc23 <julito2302mc@gmail.com>
2025-03-10 12:58:23 +0100

    feat: add win32yank

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,3 +1,15 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.g.clipboard = {
+  name = "win32yank",
+  copy = {
+    ["+"] = "/mnt/c/Users/julit/win32yank/win32yank.exe -i --crlf",
+    ["*"] = "/mnt/c/Users/julit/win32yank/win32yank.exe -i --crlf",
+  },
+  paste = {
+    ["+"] = "/mnt/c/Users/julit/win32yank/win32yank.exe -o --lf",
+    ["*"] = "/mnt/c/Users/julit/win32yank/win32yank.exe -o --lf",
+  },
+}
